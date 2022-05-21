ba3ec1a45922cd54fdfe69b68ccb408bc520d4d7
raisaazam <74347370+raisaazam@users.noreply.github.com>
2022-05-21 20:18:05 +0600

    Add files via upload

---
+++ partial-front/foot.php
@@ -0,0 +1,35 @@
+
+            <!-- footer section starts -->
+            <section class="footer">
+                <div class="container text-center">
+
+                    <span class="follow">Follow Us</span>
+                    <ul>
+                        <li>
+                            <a href="#"><img src="https://img.icons8.com/color/50/000000/facebook-new.png" /></a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="#"><img src="https://img.icons8.com/office/40/000000/instagram-new.png" /></a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="#"><img src="https://img.icons8.com/color/50/000000/twitter--v1.png" /></a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="#"><img src="https://img.icons8.com/office/40/000000/whatsapp--v2.png" /></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </section>
+
+            <!-- footer section ends -->
+
+</body>
+
+</html>
---
+++ partial-front/nav.php
@@ -0,0 +1,42 @@
+<?php include('../Admin/config.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale-1.0">
+        <title>JUST EAT</title>
+        
+        <! -- Font Awesome CSS -->
+        <link rel="stylesheet" href="CSS/all.min.css">
+        
+        <! -- Google Font -->
+         <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+        
+        <link rel ="stylesheet" href="CSS/style2.css">
+    </head>
+    <body>
+        <!-- navbar section starts -->
+       
+        <section class="navbar">
+            <div class="container">
+                
+             <nav class="navbar navbar-expand-sm  fixed-top">
+        <a href="index.php" class="navbar-brand">JUST EAT</a>
+        <span class="navbar-text">Customer's Happiness is our Aim</span>
+        
+        <div class="collapse navbar-collapse text-right" id="myMenu">
+            <ul class="navbar-nav  pl-10 px-5 custom-nav">
+                <li class="nav-item"><a href="index.php" class="nav-link">   Home</a></li>
+                <li class="nav-item"><a href="categories.php" class="nav-link">Categories</a></li>
+                <li class="nav-item"><a href="food-menu.php" class="nav-link">Food Menu</a></li>
+                <li class="nav-item"><a href="#login" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+            </ul>
+        </div >
+        </nav>
+               
+            </div>
+        </section>
+            
+        <!-- navbar section ends -->
